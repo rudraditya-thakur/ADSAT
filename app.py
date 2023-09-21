4020092cc7c16187f2a03d729c4c4cf15698e081
Rudraditya Thakur <moskva@Rudradityas-MacBook-Air.local>
2023-09-21 21:07:35 +0530

    origins changed

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ import sysinfo
 import processInfo
 import directoryStructure
 import networkInfo
+from fastapi import Request
+from sse_starlette.sse import EventSourceResponse
+import uvicorn
+from sh import tail
+from fastapi.middleware.cors import CORSMiddleware
+import time
+import os
 
 # Creating Application
 app = FastAPI()
+
+# Adding Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost","http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # Adding Routers
 app.include_router(sysinfo.router)
